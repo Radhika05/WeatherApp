26300977116ebcf4efe625155d7159b3b37cb20f
Radhika05 <bajajrids@gmail.com>
2019-07-23 17:42:32 +0530

    UI Binding

--- app/src/main/java/com/radhika/weatherapp/Fragments/CityWiseWeatherFragment.java
+++ app/src/main/java/com/radhika/weatherapp/Fragments/CityWiseWeatherFragment.java
@@ -3,7 +3,6 @@ package com.radhika.weatherapp.Fragments;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,19 +19,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.radhika.weatherapp.Adapters.CityWiseWeatherDetailsAdapter;
 import com.radhika.weatherapp.Common.Utils;
 import com.radhika.weatherapp.Models.Main;
-import com.radhika.weatherapp.Models.Weather;
 import com.radhika.weatherapp.Models.WeatherAPIForecastResult;
-import com.radhika.weatherapp.Models.WeatherAPIResult;
 import com.radhika.weatherapp.R;
 import com.radhika.weatherapp.ViewModels.WeatherViewModel;
 import com.radhika.weatherapp.databinding.FragmentCityWiseWeatherBinding;
 
-import java.io.UTFDataFormatException;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Objects;
-
-import okhttp3.internal.Util;
 
 
 public class CityWiseWeatherFragment extends Fragment {
@@ -41,25 +35,23 @@ public class CityWiseWeatherFragment extends Fragment {
     private RecyclerView rvCiyWiseData;
     private FragmentCityWiseWeatherBinding cityWiseWeatherBinding;
 
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         cityWiseWeatherBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_city_wise_weather, container, false);
-        View view =  cityWiseWeatherBinding.getRoot();
+        View view = cityWiseWeatherBinding.getRoot();
         initViews(view);
         weatherViewModel.getCity().observe(Objects.requireNonNull(getActivity()), new Observer<String>() {
             @Override
             public void onChanged(String city) {
-                Log.d("onChannges", city);
                 bindRecyclerView(city);
             }
         });
         return view;
     }
 
-    private void bindRecyclerView(String city){
+    private void bindRecyclerView(String city) {
         weatherViewModel.getWeatherForeCastData(city).observe(this, new Observer<WeatherAPIForecastResult>() {
             @SuppressLint("SetTextI18n")
             @Override
@@ -67,7 +59,6 @@ public class CityWiseWeatherFragment extends Fragment {
                 BindView(weatherAPIForecastResult);
             }
         });
-
     }
 
     @SuppressLint("SetTextI18n")
@@ -77,9 +68,9 @@ public class CityWiseWeatherFragment extends Fragment {
         List<com.radhika.weatherapp.Models.List> lists = weatherAPIForecastResult.getList();
         Main main = lists.get(0).getMain();
         cityWiseWeatherBinding.txtCity.setText(weatherAPIForecastResult.getCity().getName());
-        cityWiseWeatherBinding.txtTemp.setText(String.valueOf(Math.round(main.getTemp()))+ (char) 0x00B0);
+        cityWiseWeatherBinding.txtTemp.setText(String.valueOf(Math.round(main.getTemp())) + (char) 0x00B0);
         cityWiseWeatherBinding.txtMaxVal.setText(String.valueOf(Math.round(main.getTempMax())) + (char) 0x00B0);
-        cityWiseWeatherBinding.txtMinVal.setText(String.valueOf(Math.round(main.getTempMin()))+ (char) 0x00B0);
+        cityWiseWeatherBinding.txtMinVal.setText(String.valueOf(Math.round(main.getTempMin())) + (char) 0x00B0);
         cityWiseWeatherBinding.txtHumidityVal.setText(main.getHumidity() + " %");
         cityWiseWeatherBinding.txtWindVal.setText(lists.get(0).getWind().getSpeed() + " m/sec");
         try {
@@ -89,10 +80,10 @@ public class CityWiseWeatherFragment extends Fragment {
             e.printStackTrace();
         }
 
-        for(int i=0;i<lists.size();i++){
+        for (int i = 0; i < lists.size(); i++) {
 
         }
-        CityWiseWeatherDetailsAdapter cityWiseWeatherDetails = new CityWiseWeatherDetailsAdapter(lists,getActivity(),weatherAPIForecastResult);
+        CityWiseWeatherDetailsAdapter cityWiseWeatherDetails = new CityWiseWeatherDetailsAdapter(lists, getActivity(), weatherAPIForecastResult);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false);
         rvCiyWiseData.setLayoutManager(mLayoutManager);
         rvCiyWiseData.setItemAnimator(new DefaultItemAnimator());
@@ -104,5 +95,4 @@ public class CityWiseWeatherFragment extends Fragment {
         rvCiyWiseData = view.findViewById(R.id.rv_city_details);
         weatherViewModel = ViewModelProviders.of(Objects.requireNonNull(this.getActivity())).get(WeatherViewModel.class);
     }
-
 }
